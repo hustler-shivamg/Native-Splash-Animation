9d87e77fbd136db88036843ddf60f1cf9286bfb8
Shivam Gupta <shivam.gupta@novumlogic.com>
2023-04-14 13:57:39 +0530

    First Commit

---
+++ android/app/src/main/kotlin/com/example/native_splash_animation/MainActivity.kt
@@ -0,0 +1,9 @@
+package com.example.native_splash_animation
+
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.SplashScreen
+
+class MainActivity: FlutterActivity() {
+
+    override fun provideSplashScreen(): SplashScreen? = SplashView()
+}
